416dd99ec726b9b34b1f5c88fb665fd8ef28c1e6
Maciej Szajna <maciej.szajna@polecat.com>
2019-06-08 01:09:07 +0000

    Some tests

--- src/await_cps.clj
+++ src/await_cps.clj
@@ -2,71 +2,46 @@
   (:refer-clojure :exclude [await deref])
   (:require [clojure.walk :refer [macroexpand-all]]))
 
+(defn await [f & args]
+  (throw (new IllegalStateException "await called outside async block")))
+
 ; 'af' or 'async-form' is a function (fn [r e] form) and returns a form
 ; 'r' and 'e' are functions that given a form return a new form
 
-(defn map-af [f af]
+(defn- map-af [f af]
   (fn [r e] (af (comp r f) e)))
 
-(defn mapcat-af [f af]
+(defn- mapcat-af [f af]
   (fn [r e] (af (fn [form] ((f form) r e)) e)))
 
-(defn catch-af [f af]
-  (fn [r e] (af r (comp r f))))
-
-(defn catchcat-af [f af]
-  (fn [r e] (af r (fn [form] ((f form) r e)))))
-
-(defn inline-form [form]
+(defn- inline-form [form]
   (fn [r e] (r form)))
 
-(defn throw-form [form]
-  (fn [r e] (e form)))
-
-(defn sync-form [form]
+(defn- sync-form [form]
   (let [x (gensym "x")]
     (fn [r e] `(let [~x ~form] ~(r x)))))
 
-(defn async-form [form]
-  (let [x (gensym "x")
-        t (gensym "t")]
-    (fn [r e] `(~@form
-                 (fn [~x] (try ~(r x) (catch Throwable ~t ~(e t))))
-                 (fn [~t] ~(e t))))))
-
-(defn reduce-do [afs]
+(defn- reduce-do [afs]
   (reduce (fn ([xs x] (fn [r e] (xs (fn [xs-form] `(do ~xs-form ~(x r e))) e)))
               ([] (inline-form nil)))
           afs))
 
-(defn inline-allowed? [form]
-  (not (coll? form)))
-
-(defn reduce-call [afs]
+(defn- reduce-call [afs]
   (->> afs
-       (map #(mapcat-af (fn [form] (if (inline-allowed? form) (inline-form form) (sync-form form))) %))
+       (map #(mapcat-af (fn [form] (if (coll? form) (sync-form form) (inline-form form))) %))
        (reduce (fn [xs x] (fn [r e] (xs (fn [xs-form] (x (fn [x-form] (r `(~@xs-form ~x-form))) e)) e)))
                (inline-form ()))))
 
-(defn reduce-if [[test-af & afs]]
-  (fn [r e] (test-af (fn [test-form] `(if ~test-form ~@(map #(% r e) afs))) e)))
-
-(defn reduce-case [exp-af afs]
-  (fn [r e] (exp-af (fn [exp-form] `(case ~exp-form ~@(mapcat (fn [[case af]] (if af [case (af r e)] [(case r e)])) afs))) e)))
-
-(defn reduce-binds [binds]
-  (reduce (fn [xs [sym x]] (fn [r e] (xs (fn [_] (x (fn [x-form] `(let [~sym ~x-form] ~(r sym))) e)) e)))
-          (inline-form nil)
+(defn- reduce-binds [binds]
+  (reduce (fn [xs [sym x]] (fn [r e] (xs (fn [bound-syms] (x (fn [x-form] `(let [~sym ~x-form] ~(r (conj bound-syms sym)))) e)) e)))
+          (inline-form [])
           binds))
 
-(def ^:dynamic recur-target)
-
-(defn dbg [x] (println x) x)
+(def ^:private ^:dynamic recur-target) ; this is compile-time use only
 
 (defn walk [form]
   (let [call #(->> form (map walk) reduce-call)
         pass (inline-form form)]
-
     (cond
       (and (seq? form) (special-symbol? (first form)))
       (let [[head & tail] form]
@@ -86,12 +61,10 @@
           monitor-enter (call)
           monitor-exit (call)
 
-          let* (let [binds (->> tail first (partition 2) (map (fn [[sym form]] [sym (walk form)])) reduce-binds)
-                     body (->> tail rest (map walk) reduce-do)]
-                (->> binds (mapcat-af (fn [_] body))))
-          if (->> tail (map walk) reduce-if)
           do (->> tail (map walk) reduce-do)
-          throw (->> tail first walk (mapcat-af throw-form)) ; TODO: error on multiple tail
+
+          if (let [[test-af & alt-afs] (map walk tail)]
+               (fn [r e] (test-af (fn [test-form] `(if ~test-form ~@(map #(% r e) alt-afs))) e)))
 
           case* (let [[ge shift mask default imap & args] tail
                       default-af (walk default)
@@ -99,7 +72,22 @@
                                              (map (fn [[hash [val form]]] [hash [val ((walk form) r e)]]))
                                              (into {})))]
                   (fn [r e] `(case* ~ge ~shift ~mask ~(default-af r e) ~(imap-af r e) ~@args)))
-          
+
+          let* (let [binds-af (->> tail first (partition 2) (map (fn [[sym form]] [sym (walk form)])) reduce-binds)
+                     body-af (->> tail rest (map walk) reduce-do)]
+                 (->> binds-af (mapcat-af (fn [_] body-af))))
+
+          loop* (let [bind-names (->> tail first (partition 2) (map first))
+                      binds-af (->> tail first (partition 2) (map (fn [[sym form]] [sym (walk form)])) reduce-binds)
+                      body-af (->> tail rest (map walk) reduce-do)]
+                  (fn [r e] (binds-af (fn [syms] (binding [recur-target (gensym "rec")]
+                                                   (clojure.walk/prewalk identity ; `binding` doesn't play nice with lazy seq
+                                                     `(letfn [(~recur-target [~@bind-names] ~(body-af r e))]
+                                                        (~recur-target ~@syms))))) e)))
+
+          recur (let [rec-vals-af (->> tail (map walk) reduce-call)]
+                  (fn [r e] (rec-vals-af (fn [rec-vals-form] `(~recur-target ~@rec-vals-form)) e)))
+
           try (let [[body catches] (split-with #(not (and (seq? %) (= 'catch (first %)))) tail)
                     body-af (->> body (map walk) reduce-do)
                     catches-afs (->> catches (map (fn [[_ cls bnd & body]] [cls bnd (->> body (map walk) reduce-do)])))]
@@ -107,20 +95,21 @@
                     (let [catches (->> catches-afs (map (fn [[cls bnd catch-af]] `(catch ~cls ~bnd ~(catch-af r e)))))]
                     `(try ~(body-af r (fn [body-error] (let [t (gensym "t")]
                                                                `(try (throw ~body-error) ~@catches (catch Throwable ~t ~(e t)))))) ~@catches))))
-          loop* (let [bind-names (->> tail first (partition 2) (map first))
-                      bind-vals-af (->> tail first (partition 2) (map #(walk (second %))) reduce-call)
-                      body-af (->> tail rest (map walk) reduce-call)]
-                  (fn [r e] (bind-vals-af (fn [bind-vals-form] (binding [recur-target (gensym "rec")]
-                                                                 (clojure.walk/prewalk identity ; `binding` doesn't play nice with lazy seq
-                                                                   `(letfn [(~recur-target [~@bind-names] ~(body-af r e))]
-                                                                      (~recur-target ~@bind-vals-form))))) e)))
-          recur (let [rec-vals-af (->> tail (map walk) reduce-call)]
-                  (fn [r e] (rec-vals-af (fn [rec-vals-form] `(~recur-target ~@rec-vals-form)) e)))
+
+          throw (let [ex-af (walk (first tail))]
+                  (fn [r e] (ex-af (fn [ex-form] (e ex-form)) e)))
 
           (throw (ex-info "Unknown special form" {:unknown-special-form head}))))
   
-      (and (seq? form) (= `await (first form))) (->> form rest (map walk) reduce-call (mapcat-af async-form))
-  
+      (and (seq? form) (symbol? (first form)) (= #'await (resolve (first form))))
+      (let [call-af (->> form rest (map walk) reduce-call)]
+        (fn [r e] (call-af (fn [call-form]
+                             (let [x (gensym "x")
+                                   t (gensym "t")]
+                               `(~@call-form
+                                 (fn [~x] (try ~(r x) (catch Throwable ~t ~(e t))))
+                                 (fn [~t] ~(e t))))) e)))
+
       (seq? form) (call)
       (vector? form) (map-af vec (call))
       (map? form) (->> form (mapcat identity) (map walk) reduce-call (map-af #(->> % (partition 2) (map vec) (into {}))))
@@ -128,14 +117,11 @@
       
       :else pass)))
 
-(defn await [f & args]
-  (throw (ex-info "Await called outside async block" {})))
-
 (defn async* [form]
   (let [af (->> form macroexpand-all walk)
         r (gensym "r")
         e (gensym "e")]
-      `(fn [~r ~e] ~(af (fn [form] `(~r ~form)) (fn [form] `(~e ~form))))))
+    `(fn [~r ~e] ~(af (fn [form] `(~r ~form)) (fn [form] `(~e ~form))))))
 
 (defmacro async
   ([form]
--- test/await_cps_test.clj
+++ test/await_cps_test.clj
@@ -1,45 +1,59 @@
-(ns await-cps-test)
+(ns await-cps-test
+  (:refer-clojure :exclude [await deref])
+  (:require [clojure.test :refer :all]
+            [await-cps :refer [async await deref]]))
 
 ; The goal of this library is to enable writing CPS (continuation-passing style)
 ; code preserving the flow of synchronous code. It achieves that elevating
 ; the CPS calls deep in a form all the way up
 
-; Ideally, synchronous code would map to itself. Transforming a form,
-; any subform that doesn't do CPS should be presesrved as well.
-; The async2-macro doesn't do it, favouring the simplicity of implementation.
-; I did have a go at trying to preserve forms before but got nowhere
+(defn successful-async [v r e]
+  (future (r v)))
 
-(comment
-  (when (sync? a)
-    (= (transform a) [:sync a]))
-  (when (async? a)
-    (= (transform a) [:async a]))
-  (when (sync? a)
-    (= (transform (await a)) [:async a]))
-  (when (and (sync? a) (sync? b))
-    (= (transform (f a b)) [:sync (f a b)]))
-  (when (and (async? a) (sync? b))
-    (= (transform (f a b)) [:async (fn [r e] (a (fn [av] (r (f av b))) e))]))
-  (when (and (sync? a) (async? b))
-    (= (transform (f a b)) [:async (fn [r e] (let [av a] (b (fn [bv] (r (f av bv))) e)))]))
-  (when (and (async? a) (async? b))
-    (= (transform (f a b)) [:async (fn [r e] (a (fn [av] (b (fn [bv] (r (f av bv))) e)) e))]))
+(defn failing-async [r e]
+  (future (e (ex-info "failing" {}))))
 
-  (do (await a) b) => (a (fn [_] (r b)) e)
-  (do a (await c)) => (do a (c (fn [cv] (r cv)) e))
-  
-  (do a) => (r a)
-  (do (await a) & xs) => (~a (fn [_] (do ~@xs)))
-  (do (await a)) => (~a (fn [av] (r av)))
-  
-  (async (loop [a 1] (if (await f a) (recur (inc a)) a))) => ((fn rec [a] (f a (fn [fv] (if fv (rec (inc a)) (r a))) e)) 1)
-  
-  (async (try (await f) (catch Exception e a))) => (fn [r e] (try (f (fn [fv] (r fv))
-                                                                     (fn [ex] (try (throw ex)
-                                                                                   (catch Exception e (r a))
-                                                                                   (catch Throwable t (e t)))))
-                                                               (catch Exception e (r a))
-                                                               (catch Throwable t (e t))))
-  
-  (async [(println "1") (throw ex)]) ??
-)
+(defn immed-failing-async [r e]
+  (throw (ex-info "immed-failing" {})))
+
+(deftest async-await
+  (are [a b] (= a (deref (async b)))
+    1 1
+    1 (await successful-async 1)
+
+    1 (try (await failing-async) (catch clojure.lang.ExceptionInfo e 1))
+    1 (try (await immed-failing-async) (catch clojure.lang.ExceptionInfo e 1))
+
+    1 (if (await successful-async false) 0 1)
+    1 (if true (await successful-async 1) 0)
+    1 (if false 0 (await successful-async 1))
+
+    1 (case (await successful-async 1) 1 1 2)
+    1 (case 0 0 (await successful-async 1) 2)
+    1 (case 0 2 2 (await successful-async 1))
+
+    1 (let [x 0 y (await successful-async x) z (inc y)] z)
+    1 (let [a 1] (await successful-async a))
+    1 (let [a 1] (await successful-async 2) (await successful-async 1))
+
+    1 (loop [a 10] (if (= a 1) a (recur (await successful-async (dec a)))))
+    1 (loop [a 0 b (inc a)] (await successful-async b))
+
+    1 ((await successful-async inc) 0)
+    [1 2] [(await successful-async 1) 2]
+    #{1 2} #{(await successful-async 1) 2}
+    {1 2 3 4} {(await successful-async 1) 2 3 (await successful-async 4)}
+  ))
+
+(deftest async-await-side-effects
+  (let [efs (atom nil)
+        eff (fn [n] (swap! efs conj n))
+        fail (fn [n r e] (eff n) (e (ex-info "err" {})))
+        succ (fn [n v r e] (eff n) (r v))
+        effects (fn [as] (reset! efs []) (try (deref as) (catch Throwable t nil)) @efs)]
+    (are [a b] (= a (effects (async b)))
+      ["a"] (eff "a")
+      ["a"] (let [x (await fail "a") z (eff "b")] (eff "c"))
+      ["a" "c"] (if (await succ "a" false) (eff "b") (eff "c"))
+      ["a" "c"] (if (await succ "a" false) (eff "b") (eff "c"))
+    )))
