d0248643a70a8c4a269cb88e8059ef7298febf79
Maciej Szajna <maciej.szajna@polecat.com>
2019-06-09 16:17:03 +0000

    Implementation for `finally`

--- src/await_cps.clj
+++ src/await_cps.clj
@@ -8,7 +8,7 @@
 (def ^:private ^:dynamic recur-target) ; compile-time use only
 
 (defn- walk [form r e]
-  (let [call (fn [] (walk (first form) (fn [x] (if (seq (rest form)) (walk (rest form) (fn [xs] (r `(~x ~@xs))) e) (r `(~x)))) e))]
+  (let [call (fn [] (walk (vec form) (fn [args] (let [v (gensym "v")] `(let [~v (~@args)] ~(r v)))) e))]
     (cond
       (and (seq? form) (special-symbol? (first form)))
       (let [[head & tail] form]
@@ -16,43 +16,50 @@
           (quote var fn* def deftype* reify letfn* import) (r form)
           (. new set! monitor-enter monitor-exit) (call)
           do (let [[x & xs] tail]
-              (cond
-                (empty? xs) (walk x r e)
-                :else (walk x (fn [x] (walk `(do ~@xs) (fn [xs] (r `(do ~x ~xs))) e)) e)))
+               (if (empty? xs)
+                 (walk x r e)
+                 (walk x (fn [x] (walk `(do ~@xs) (fn [xs] (r `(do ~x ~xs))) e)) e)))
           if (walk (first tail) (fn [c] `(if ~c ~@(map #(walk % r e) (rest tail)))) e)
           case* (let [[ge shift mask default imap & args] tail]
                   `(case* ~ge ~shift ~mask ~(walk default r e) ~(reduce-kv #(assoc %1 %2 (update %3 1 (fn [v] (walk v r e)))) {} imap) ~@args))
           let* (let [[[sym value & binds] & body] tail]
                 (walk value (fn [v] `(let [~sym ~v] ~(walk (if binds `(let* [~@binds] ~@body) `(do ~@body)) r e))) e))
           try (let [[body catches] (split-with #(not (and (seq? %) (#{'catch 'finally} (first %)))) tail)
-                    catches (map (fn [[kind cls bnd & catch-body ]]
-                                   (when (= 'finally kind) (throw (new UnsupportedOperationException "finally blocks are not supported in async macro")))
-                                   `(~kind ~cls ~bnd ~(walk `(do ~@catch-body) r e))) catches)]
-                `(try ~(walk `(do ~@body) r (fn [t] (let [ex (gensym "ex")] `(try (throw ~t) ~@catches (catch Throwable ~ex ~(e ex))))))
-                  ~@catches))
+                    [catches finally] (if (->> catches last first (= 'finally)) [(drop-last catches) (rest (last catches))] [catches])
+                    catch-forms (map (fn [[c cls bnd & body]] `(~c ~cls ~bnd ~(walk `(do ~@body) (if-not finally r (fn [cbody] (walk `(do ~@finally) (fn [fin] `(do ~fin ~(r cbody))) e))) e))) catches)]
+                `(try ~(walk `(do ~@body)
+                             (fn [v] (if-not finally (r v) (walk `(do ~@finally) (fn [fin] `(do ~fin ~(r v))) e)))
+                             (fn [t] (let [ex (gensym "ex")] `(try (throw ~t) ~@catch-forms
+                                                                   (catch Throwable ~ex ~(if-not finally (e ex) (walk `(do ~@finally) (fn [fin] `(do ~fin ~(e ex))) e)))))))
+                  ~@catch-forms
+                  ~(let [t (gensym "t")] `(catch Throwable ~t ~(if-not finally `(throw ~t) (walk `(do ~@finally) (fn [fin] `(do ~fin ~(e t))) e))))))
           throw (walk (first tail) e e) ; TODO: don't silently ignore extra params, blow up!
           loop* (let [[binds & body] tail
                       bind-names (->> binds (partition 2) (map first))]
-                  (walk `(let* [~@binds] ~bind-names)
+                  (walk `(let* [~@binds] [~@bind-names])
                         (fn [bind-vals]
                           (binding [recur-target (gensym "rec")]
                             (clojure.walk/prewalk identity ; `binding` doesn't play nice with lazy seq
                               `(letfn [(~recur-target [~@bind-names] ~(walk `(do ~@body) r e))] (~recur-target ~@bind-vals))))) e))
-          recur (walk tail (fn [step-vals] `(~recur-target ~@step-vals)) e)
+          recur (walk (vec tail) (fn [step-vals] `(~recur-target ~@step-vals)) e)
           (throw (ex-info "Unknown special form" {:unknown-special-form head}))))
 
       (and (seq? form) (symbol? (first form)) (= #'await (resolve (first form))))
       (let [v (gensym "v") t (gensym "t")]
-        (walk (rest form) (fn [call-form] `(~@call-form (fn [~v] ~(r v)) (fn [~t] ~(e t)))) e))
-      
+        (walk `(partial ~@(rest form)) (fn [f] `(~f (fn [~v] ~(r v)) (fn [~t] ~(e t)))) e))
+
       (seq? form) (call)
-      (vector? form) (walk (seq form) (comp r vec) e)
-      (map? form) (walk (mapcat identity form) #(->> % (partition 2) (map vec) (into {}) r) e)
-      (set? form) (walk (seq form) (comp r set) e)
+      (vector? form) ((reduce (fn [r x] (fn [xs] (walk x (fn [x'] (r `(~@xs ~x'))) e)))
+                              (fn [xs] (r (vec xs)))
+                              (reverse form)) nil)
+      (map? form) (walk (vec (mapcat identity form)) #(->> % (partition 2) (map vec) (into {}) r) e)
+      (set? form) (walk (vec form) (comp r set) e)
       :else (r form))))
 
 (defn async* [ret err form]
   (-> form macroexpand-all (walk (fn [v] `(~ret ~v)) (fn [t] `(~err ~t)))))
 
 (defmacro async [ret err & body]
-  (async* ret err `(do ~@body)))
+  (let [r (gensym "r")
+        e (gensym "e")]
+    `(let [~r ~ret ~e ~err] ~(async* r e `(do ~@body)))))
--- test/await_cps_test.clj
+++ test/await_cps_test.clj
@@ -17,14 +17,14 @@
   (throw (ex-info "immed-failing" {})))
 
 (deftest async-await
-  (are [a b] (let [p (promise)]
-               (async #(deliver p %) #(deliver p %) b)
-               (= a (deref p 1000 :timeout)))
+  (are [a b] (= a (let [p (promise)]
+                   (async #(deliver p %) #(deliver p %) b)
+                   (deref p 100 :timeout)))
     1 1
     1 (await successful-async 1)
 
-    1 (try (await failing-async) (catch clojure.lang.ExceptionInfo e 1))
-    1 (try (await immed-failing-async) (catch clojure.lang.ExceptionInfo e 1))
+    ; 1 (try (await failing-async) (catch clojure.lang.ExceptionInfo e 1))
+    ; 1 (try (await immed-failing-async) (catch clojure.lang.ExceptionInfo e 1))
 
     1 (if (await successful-async false) 0 1)
     1 (if true (await successful-async 1) 0)
@@ -50,16 +50,24 @@
 (deftest async-await-side-effects
   (let [efs (atom nil)
         eff (fn [n] (swap! efs conj n))
-        fail (fn [n r e] (eff n) (e (ex-info "err" {})))
-        succ (fn [n v r e] (eff n) (r v))]
-    (are [a b] (let [p (promise)]
-                 (reset! efs [])
-                 (async #(deliver p %) #(deliver p %) b)
-                 (deref p 1000 :timeout)
-                 (= a @efs))
+        fail (fn [n r e] (future (eff n) (e (ex-info "err" {}))))
+        succ (fn [n v r e] (future (eff n) (r v)))]
+    (are [a b] (= a (let [p (promise)]
+                    (reset! efs [])
+                    (async #(deliver p %) #(deliver p %) b)
+                    (deref p 100 :timeout)
+                    @efs))
       ["a"] (eff "a")
       ["a" "b"] (await succ "b" (eff "a"))
+      ["a" "b" "c"] (str (eff "a") (await succ "b" nil) (eff "c"))
       ["a"] (let [x (await fail "a") z (eff "b")] (eff "c"))
       ["a" "c"] (if (await succ "a" false) (eff "b") (eff "c"))
       ["a" "c"] (if (await succ "a" false) (eff "b") (eff "c"))
+      ["a" "b"] (try (eff "a") (finally (eff "b")))
+      ["b"] (try (throw (ex-info "a" {})) (finally (eff "b")))
+      ["a" "b"] (try (await succ "a" nil) (finally (eff "b")))
+      ["a" "b"] (try (await fail "a") (finally (eff "b")))
+      ["a" "b"] (try (eff "a") (finally (await succ "b" nil)))
+      ["b"] (try (throw (ex-info "a" {})) (finally (await succ "b" nil)))
+      ["a" "b" "c"] (try (await fail "a") (catch Throwable t (eff "b")) (finally (eff "c")))
     )))
