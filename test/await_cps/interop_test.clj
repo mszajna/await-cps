4bc42f239144f882df84a3879e409eb413ba05a2
Maciej Szajna <maciej.szajna@polecat.com>
2019-11-02 18:13:06 +0000

    Interoperability + simplified README
    Added interoperability with blocking code.
    Added interoperability namespace for Java 8 CompletableFuture.
    Documented interoperability with Manifold, promesa and core.async.
    Removed the handling of all exceptions thrown in callbacks. This is now responsibility of interop code.
    Documented that user is expected not to throw in their callbacks.
    Reduced the size of README using more concise wording and moving some topics to docstrings.

---
+++ test/await_cps/interop_test.clj
@@ -0,0 +1,54 @@
+(ns await-cps.interop-test
+  (:refer-clojure :exclude [await])
+  (:require [clojure.test :refer :all]
+            [await-cps :refer [afn await either-promise]]
+            [await-cps.java :as j]
+            [manifold.deferred :as d]
+            [clojure.core.async :as a])
+  (:import java.util.concurrent.CompletableFuture
+           java.util.concurrent.TimeUnit
+           java.util.concurrent.TimeoutException
+           java.util.concurrent.ExecutionException))
+
+(defn await-at-most! [ms cps-fn & args]
+  (let [p (apply either-promise cps-fn args)
+        [v t] (deref p ms [nil (new TimeoutException)])]
+    (if t (throw t) v)))
+
+(deftest use-java-cf-in-async-function
+  (let [cf (new CompletableFuture)]
+    (.complete cf true)
+    (is (await-at-most! 200 (afn [] (await j/completed cf)))))
+  (let [cf (new CompletableFuture)]
+    (.completeExceptionally cf (ex-info "Success" {}))
+    (is (thrown-with-msg? clojure.lang.ExceptionInfo #"Success"
+          (await-at-most! 200 (afn [] (await j/completed cf)))))))
+
+(deftest use-cps-with-java-cf
+  (let [cf (j/future-call (afn [] true))]
+    (is (deref cf 200 :timeout)))
+  (let [cf (j/future-call (afn [] (throw (ex-info "Success" {}))))]
+    (is (thrown-with-msg? ExecutionException #"Success"
+          (deref cf 200 :timeout)))))
+
+(deftest use-manifold-deferred-in-async-function
+  (let [d (d/success-deferred true)]
+    (is (await-at-most! 200 (afn [] (await d/on-realized d)))))
+  (let [d (d/error-deferred (ex-info "Success" {}))]
+    (is (thrown-with-msg? clojure.lang.ExceptionInfo #"Success"
+          (await-at-most! 200 (afn [] (await d/on-realized d)))))))
+
+(deftest use-cps-with-manifold-deferred
+  (let [d (d/chain (j/future-call (afn [] true)))]
+    (is (deref d 200 :timeout)))
+  (let [d (d/chain (j/future-call (afn [] (throw (ex-info "Success" {})))))]
+    (is (thrown-with-msg? clojure.lang.ExceptionInfo #"Success"
+          (deref d 200 :timeout)))))
+
+(deftest use-core-async-channel-in-async-function
+  (let [ch (a/chan 1)]
+    (is (await-at-most! 200
+          (afn [] (when (await a/put! ch true) (await a/take! ch))))))
+  (let [ch (a/chan)]
+    (is (nil? (await-at-most! 200
+                (afn [] (a/close! ch) (await a/take! ch)))))))
